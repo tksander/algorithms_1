9f0f1bebebbee5fab70480ee127672258f26d993
tksander <tkepasander@gmail.com>
2016-11-28 22:04:16 -0800

    Initial commit - Algorithms 1

---
+++ Week_1/QuickUnionUF.java
@@ -0,0 +1,31 @@
+public class QuickUnionUF
+{
+    private int[] id;
+    
+    // intialize id of each object to itself
+    public QuickUnionUF(int N)
+    {
+        id = new int[N];
+        for (int i = 0; i < N; i++) id[i] = i;
+    }
+    
+    // chases parent pointers until reaches root
+    private int root(int i)
+    {
+        while (i != id[i]) i = id[i];
+        return i;
+    }
+    
+    // check if p and q have same root
+    public boolean connected(int p, int q)
+    {
+        return root(p) == root (q);
+    }
+    
+    public void union(int p, int q)
+    {
+        int i = root(p);
+        int j = root(q);
+        id[i] = j;
+    }
+}
---
+++ testprograms/UseArgument.java
@@ -0,0 +1,11 @@
+public class UseArgument {
+
+    public static void main(String[] args) {
+       String a, b, c;
+       a  = "Hello,";
+       b = " Bob";
+       c = a + b;
+       System.out.println(c);
+    }
+
+}
