df8a8869e191a6b83fb7d73b62382909267b0071
tksander <tkepasander@gmail.com>
2016-12-13 18:45:03 -0800

    Added more tests for Full sites

--- assignments/test/PercolationTest.java
+++ assignments/test/PercolationTest.java
@@ -1,5 +1,6 @@
 import static com.sun.xml.internal.ws.dump.LoggingDumpTube.Position.Before;
 import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,47 +16,47 @@ public class PercolationTest {
 
     @Test
     public void shouldHaveAllClosedSites() {
-        assertFalse(grid.isOpen(1,1));
+        assertFalse(grid.isOpen(1, 1));
     }
 
     @Test
     public void shouldNotBeFullIfNotOpenSite() {
-        assertFalse(grid.isFull(1,1));
+        assertFalse(grid.isFull(1, 1));
     }
 
     @Test
     public void shouldHaveFullSites() {
-        grid.open(1,1);
-        grid.open(2,1);
-        grid.open(3,1);
-        grid.open(4,1);
-        assertTrue(grid.isFull(4,1));
+        grid.open(1, 1);
+        grid.open(2, 1);
+        grid.open(3, 1);
+        grid.open(4, 1);
+        assertTrue(grid.isFull(4, 1));
     }
 
     @Test
     public void shouldOpenASite() {
-        grid.open(1,1);
-        assertTrue(grid.isOpen(1,1));
-        grid.open(1,2);
-        assertTrue(grid.isOpen(1,2));
+        grid.open(1, 1);
+        assertTrue(grid.isOpen(1, 1));
+        grid.open(1, 2);
+        assertTrue(grid.isOpen(1, 2));
     }
 
     @Test
     public void shouldPercolateLinear() {
-        grid.open(1,1);
-        grid.open(2,1);
-        grid.open(3,1);
-        grid.open(4,1);
+        grid.open(1, 1);
+        grid.open(2, 1);
+        grid.open(3, 1);
+        grid.open(4, 1);
         assertTrue(grid.percolates());
     }
 
     @Test
     public void shouldPercolateSidePath() {
-        grid.open(1,1);
-        grid.open(2,1);
-        grid.open(2,2);
-        grid.open(3,2);
-        grid.open(4,2);
+        grid.open(1, 1);
+        grid.open(2, 1);
+        grid.open(2, 2);
+        grid.open(3, 2);
+        grid.open(4, 2);
         assertTrue(grid.percolates());
     }
 }
