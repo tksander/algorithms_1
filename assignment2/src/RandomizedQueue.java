db2bc04b2617cc194e59af7601a6f13d1a77b3dd
tksander <tkepasander@gmail.com>
2016-12-24 13:14:36 -0800

    RandomizedQueue completed. Writing unit tests.

--- assignment2/src/RandomizedQueue.java
+++ assignment2/src/RandomizedQueue.java
@@ -1,13 +1,12 @@
 import java.util.Iterator;
+
+import edu.princeton.cs.algs4.*;
+import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdRandom.*;
 
 /**
  * Created by tommysander on 12/12/16.
  * The order of two or more iterators to the same randomized queue must be mutually independent; each iterator must maintain its own random order.
- * Throw a java.lang.NullPointerException if the client attempts to add a null item;
- * throw a java.util.NoSuchElementException if the client attempts to sample or dequeue an item from an empty randomized queue;
- * throw a java.lang.UnsupportedOperationException if the client calls the remove() method in the iterator;
- * throw a java.util.NoSuchElementException if the client calls the next() method in the iterator and there are no more items to return.
  */
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
@@ -40,11 +39,9 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
      * add the item
      */
     public void enqueue(Item item) {
+        if (item == null) throw new java.lang.NullPointerException();
         if (size == queue.length) { this.resize(2 * queue.length);}
-        // Shuffle on each enqueu
-        // Uses Knuth shuffle with O(n())
             queue[size++] = item;
-        StdRandom.shuffle(queue);
     }
 
     /**
@@ -52,19 +49,61 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
      */
     public Item dequeue() {
         if(this.isEmpty()) { throw new java.util.NoSuchElementException("No items in queue");}
+        int randomIndex = StdRandom.uniform(size);
+        Item temp = queue[randomIndex];
+        queue[randomIndex] = queue[size - 1];
+        queue[size - 1] = null;
+        size--;
         // shrink size of array if necessary
+        if (size > 0 && size < (queue.length / 4)) this.resize(queue.length / 2);
+        return temp;
     }
 
     /**
      *  return (but do not remove) a random item
      */
     public Item sample() {
+        if(this.isEmpty()) { throw new java.util.NoSuchElementException("No items in queue");}
+        int index = StdRandom.uniform(0, size);
+        return queue[index];
     }
 
     /**
      *  return an independent iterator over items in random order
      */
     public Iterator<Item> iterator() {
+        return new RandomizedQueueIterator();
+    }
+
+    private class RandomizedQueueIterator implements Iterator<Item> {
+        private int i;
+        private int iteratorSize;
+        private Item[] iteratorQueue;
+
+
+        public void RadomizeQueueIterator() {
+            iteratorQueue = (Item[]) new Object[size];
+            for (int i = 0; i < size; i++) {
+                iteratorQueue[i] = queue[i];
+            }
+            iteratorSize = size;
+        }
+
+        public boolean hasNext() {
+            return i < iteratorSize;
+        }
+        public Item next() {
+            if(!this.hasNext()) { throw new java.util.NoSuchElementException("No more items");}
+            int randomIndex = StdRandom.uniform(iteratorSize);
+            Item temp = iteratorQueue[randomIndex];
+            iteratorQueue[randomIndex] = iteratorQueue[iteratorSize - 1];
+            iteratorQueue[iteratorSize - 1] = null;
+            iteratorSize--;
+            return temp;
+        }
+        public void remove() {
+            throw new java.lang.UnsupportedOperationException("Remove not available");
+        }
     }
 
     /**
@@ -72,6 +111,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
      */
     public static void main(String[] args) {
     }
+
     private void resize(int capacity) {
        Item[] temp = (Item[]) new Object[capacity];
         for (int i = 0; i < size; i++) {
---
+++ assignment2/test/RandomizedQueueTest.java
@@ -0,0 +1,66 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by tommysander on 12/24/16.
+ */
+public class RandomizedQueueTest {
+    private RandomizedQueue<Integer> queue;
+
+
+    @Before
+    public void setUp() throws Exception {
+        queue = new RandomizedQueue<Integer>();
+    }
+
+    @Test
+    public void testIsEmpty() throws Exception {
+        assertTrue(queue.isEmpty());
+    }
+
+    @Test
+    public void testSize() throws Exception {
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        int size = queue.size();
+        assertSame(size, 3);
+    }
+
+    @Test
+    public void testEnqueue() throws Exception {
+
+    }
+
+    @Test
+    public void testDequeue() throws Exception {
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.enqueue(5);
+        int item = queue.dequeue();
+        System.out.println(item);
+        assertNotSame(item, 1);
+        assertSame(queue.size(), 4);
+    }
+
+    @Test
+    public void testSample() throws Exception {
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.enqueue(5);
+        int item = queue.sample();
+        System.out.println(item);
+        assertNotSame(item, 1);
+    }
+
+    @Test
+    public void testIterator() throws Exception {
+
+    }
+}
